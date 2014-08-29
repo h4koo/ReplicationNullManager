8a91454e712995557f92355e665fea828e3e5b51
pabloariasmora <pabloariasmora@gmail.com>
2014-08-29 13:19:57 -0600

    Test Commit Access

--- ReplicationManager/Program.cs
+++ ReplicationManager/Program.cs
@@ -17,6 +17,7 @@ namespace ReplicationManager
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
